2b2fd3942c0163aa1e6fe42c4cbe903f1163d893
kostya300 <kostya.barnung@gmail.com>
2026-04-24 21:24:36 +0500

    alotofchange3

--- store/models.py
+++ store/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+from users.models import User
+
+
 class Category(models.Model):
     name = models.CharField('Название', max_length=100, unique=True)
     slug = models.SlugField('URL', unique=True)
@@ -58,6 +61,54 @@ class Product(models.Model):
         if self.old_price and self.old_price > self.price:
             return round((1 - self.price / self.old_price) * 100)
         return 0
+
+class Basket(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Пользователь'
+    )
+    product = models.ForeignKey(  # ← не products, а product
+        Product,
+        on_delete=models.CASCADE,
+        verbose_name='Товар'
+    )
+    quantity = models.PositiveIntegerField(default=1)  # Лучше default=1, а не 0
+    created_timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Элемент корзины'
+        verbose_name_plural = 'Корзина'
+        ordering = ['created_timestamp']
+        unique_together = ('user', 'product')  # Запрещает дубли
+
+    def __str__(self):
+        return f'Корзина: {self.user.email} | {self.product.name} × {self.quantity}'
+
+    @property
+    def total_price(self):
+        """Общая стоимость позиции"""
+        return self.quantity * self.product.price
+
+    @property
+    def name(self):
+        return self.product.name
+
+    @property
+    def price(self):
+        return self.product.price
+
+    @property
+    def image(self):
+        """Возвращает основное или первое изображение товара"""
+        main_img = self.product.images.filter(is_main=True).first()
+        if main_img:
+            return main_img.image.url
+        first_img = self.product.images.first()
+        if first_img:
+            return first_img.image.url
+        return '/static/images/no-image.png'  # заглушка
+
 # модель ProductImage для хранения дополнительных изображений.
 class ProductImage(models.Model):
     product = models.ForeignKey(Product,related_name='images',on_delete=models.CASCADE,verbose_name='Товар')
